692581da94c89af6520749efdffa3ded55ff8561
Brandon H. Goding <brandon.h.goding@gmail.com>
2024-11-16 16:35:20 -0500

    feature(black): precommit hook

--- www_softwaremaine_com/urls.py
+++ www_softwaremaine_com/urls.py
@@ -24,7 +24,7 @@ from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
-    path('cms/', include(wagtailadmin_urls)),
-    path('documents/', include(wagtaildocs_urls)),
-    path('pages/', include(wagtail_urls)),
+    path("cms/", include(wagtailadmin_urls)),
+    path("documents/", include(wagtaildocs_urls)),
+    path("pages/", include(wagtail_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
