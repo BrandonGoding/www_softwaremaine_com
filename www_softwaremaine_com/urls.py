99df1ac01d0cd2d46486f47fed0282f4c5a0b90a
Brandon H. Goding <brandon.h.goding@gmail.com>
2024-12-03 22:00:43 -0500

    feature(urls): cms only shows up under debug

--- www_softwaremaine_com/urls.py
+++ www_softwaremaine_com/urls.py
@@ -22,8 +22,14 @@ from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
-urlpatterns = [
-    path("cms/", include(wagtailadmin_urls)),
+urlpatterns = []
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("cms/", include(wagtailadmin_urls)),
+    ]
+
+urlpatterns += [
     path("documents/", include(wagtaildocs_urls)),
     path("", include(wagtail_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
